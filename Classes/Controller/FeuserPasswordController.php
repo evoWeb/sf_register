f9056c87ef5fa9bacc0eac2b071bf4219153d8dd
sfischer <sfischer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-29 20:30:42 +0000

    Add some error handling for empty password
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sf_register/trunk@45923 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/FeuserPasswordController.php
+++ Classes/Controller/FeuserPasswordController.php
@@ -29,28 +29,74 @@ class Tx_SfRegister_Controller_FeuserPasswordController extends Tx_SfRegister_Co
 	/**
 	 * Form action
 	 *
-	 * @return void
+	 * @return string An HTML form
 	 */
 	public function formAction() {
+		$errors = $this->request->getErrors();
+		debug($errors['oldPassword']->getMessage());
 	}
 
 	/**
 	 * Save action
 	 *
-	 * @param array $passwords
+	 * @param Tx_SfRegister_Domain_Model_FrontendUser $user
+	 * @param string $passwordAgain
+	 * @param string $oldPassword
 	 * @return void
-	 * @validate $passwords Tx_SfRegister_Domain_Validator_PasswordsValidator(minimum = 8, maximum = 40)
+	 * @validate $user Tx_SfRegister_Domain_Validator_UserValidator(type = password)
+	 * @validate $passwordAgain Tx_SfRegister_Domain_Validator_PasswordAgainValidator
+	 * @dontvalidate $oldPassword
 	 */
-	public function saveAction(array $passwords) {
-		$password = $this->encryptPassword($passwords['newPassword1']);
-
+	public function saveAction(Tx_SfRegister_Domain_Model_FrontendUser $user, $passwordAgain, $oldPassword) {
 		if ($GLOBALS['TSFE']->fe_user->user != FALSE) {
 			$user = $this->userRepository->findByUid($GLOBALS['TSFE']->fe_user->user['uid']);
 
-			$user->setPassword($password);
+			if ($this->checkOldpassword($oldPassword, $user)) {
+				$password = $this->encryptPassword($passwordAgain);
 
-			$this->userRepository->update($user);
+				$user->setPassword($password);
+
+				$this->userRepository->update($user);
+			}
 		}
+	}
+
+	/**
+	 * Check if the old password needs to be validated
+	 * Check if the old password is empty
+	 * Check if the old password is unequal the password in the user object
+	 *
+	 * @param string $oldPassword
+	 * @param Tx_SfRegister_Domain_Model_FrontendUser $user
+	 * @return boolean
+	 */
+	protected function checkOldpassword($oldPassword, Tx_SfRegister_Domain_Model_FrontendUser $user) {
+		$result = TRUE;
+
+		if (!empty($this->settings['checkOldPasswordOnChange'])) {
+			$error = NULL;
+
+			if ($oldPassword === '') {
+				$error = t3lib_div::makeInstance('Tx_Extbase_Validation_Error', Tx_Extbase_Utility_Localization::translate('error.oldpassword.empty', 'SfRegister'), 1301429597);
+				$result = FALSE;
+			} elseif ($oldPassword !== $user->getPassword()) {
+				$error = t3lib_div::makeInstance('Tx_Extbase_Validation_Error', Tx_Extbase_Utility_Localization::translate('error.oldpassword.notequal', 'SfRegister'), 1301429598);
+				$result = FALSE;
+			}
+
+			if ($result === FALSE) {
+				$errors = $this->request->getErrors();
+				$errors['oldPassword'] = $error;
+				$this->request->setErrors($errors);
+
+				if ($this->request->hasArgument('__referrer')) {
+					$referrer = $this->request->getArgument('__referrer');
+					$this->forward($referrer['actionName'], $referrer['controllerName'], $referrer['extensionName'], $this->request->getArguments());
+				}
+			}
+		}
+
+		return $result;
 	}
 }
 
