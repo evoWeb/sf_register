bff9022e7fcb9f9e1da67c7270c8f7a685b73d88
sfischer <sfischer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-30 14:38:01 +0000

    Add more fields
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sf_register/trunk@47145 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -25,6 +25,51 @@ $tempColumns = array(
 		)
 	),
 
+	'gender' => array(
+		'exclude' => 1,
+		'label' => 'LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.gender',
+		'config' => array(
+			'type' => 'radio',
+			'items' => array(
+				Array('LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.gender.I.1', '1'),
+				Array('LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.gender.I.2', '2')
+			),
+		)
+	),
+	'date_of_birth' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.date_of_birth',
+		'config' => array(
+			'type' => 'input',
+			'size' => '10',
+			'max' => '20',
+			'eval' => 'date',
+			'checkbox' => '0',
+			'default' => ''
+		)
+	),
+	'language' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.language',
+		'config' => array(
+			'type' => 'input',
+			'size' => '4',
+			'max' => '2',
+			'eval' => '',
+			'default' => ''
+		)
+	),
+	'zone' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.zone',
+		'config' => array(
+			'type' => 'input',
+			'size' => '20',
+			'max' => '40',
+			'eval' => 'trim',
+			'default' => ''
+		)
+	),
 	'mobilephone' => array(
 		'exclude' => 1,
 		'label' => 'LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.mobilephone',
@@ -40,10 +85,100 @@ $tempColumns = array(
 			'type' => 'check'
 		)
 	),
+	'status' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.status',
+		'config' => array(
+			'type' => 'select',
+			'items' => array(
+				Array('LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.status.I.0', '0'),
+				Array('LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.status.I.1', '1'),
+				Array('LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.status.I.2', '2'),
+				Array('LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.status.I.3', '3'),
+				Array('LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.status.I.4', '4'),
+			),
+			'size' => 1,
+			'maxitems' => 1,
+		)
+	),
+	'by_invitation' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.by_invitation',
+		'config' => array(
+			'type' => 'check',
+			'default' => '0'
+		)
+	),
+	'comments' => array(
+		'exclude' => 0,
+		'label' => 'LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.comments',
+		'config' => array(
+			'type' => 'text',
+			'rows' => '5',
+			'cols' => '48'
+		)
+	),
+	'module_sys_dmail_html' => array(
+		'label' => 'LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.module_sys_dmail_html',
+		'exclude' => '1',
+		'config' => array(
+			'type'=>'check'
+		)
+	),
 );
+
 t3lib_extMgm::addTCAcolumns('fe_users', $tempColumns, 1);
+t3lib_extMgm::addToAllTCAtypes('fe_users', 'gender', '', 'before:name');
+t3lib_extMgm::addToAllTCAtypes('fe_users', 'date_of_birth, language, status', '', 'after:name');
+t3lib_extMgm::addToAllTCAtypes('fe_users', 'zone', '', 'after:city');
 t3lib_extMgm::addToAllTCAtypes('fe_users', 'mobilephone', '', 'after:telephone');
 t3lib_extMgm::addToAllTCAtypes('fe_users', 'mailhash, gtc');
+
+t3lib_extMgm::addToAllTCAtypes('fe_users', 'by_invitation, comments, module_sys_dmail_html');
+
+if (t3lib_extMgm::isLoaded('static_info_tables')) {
+	$tempColumns = array(
+		'static_info_country' => array(
+			'exclude' => 0,
+			'label' => 'LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.static_info_country',
+			'config' => array(
+				'type' => 'input',
+				'size' => '5',
+				'max' => '3',
+				'eval' => '',
+				'default' => ''
+			)
+		),
+	);
+	t3lib_extMgm::addTCAcolumns('fe_users', $tempColumns, 1);
+	t3lib_extMgm::addToAllTCAtypes('fe_users', 'static_info_country', '', 'after:zone');
+}
+
+if (t3lib_extMgm::isLoaded('direct_mail')) {
+	$tempColumns = array(
+		'module_sys_dmail_category' => array(
+			'label' => 'LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:fe_users.module_sys_dmail_group.category',
+			'exclude' => '1',
+			'config' => array(
+				'type' => 'select',
+				'foreign_table' => 'sys_dmail_category',
+				'foreign_table_where' => 'AND sys_dmail_category.l18n_parent=0 AND sys_dmail_category.pid IN (###PAGE_TSCONFIG_IDLIST###) ORDER BY sys_dmail_category.uid',
+				'itemsProcFunc' => 'tx_directmail_select_categories->get_localized_categories',
+				'itemsProcFunc_config' => array(
+					'table' => 'sys_dmail_category',
+					'indexField' => 'uid',
+				),
+				'size' => 5,
+				'minitems' => 0,
+				'maxitems' => 60,
+				'renderMode' => 'checkbox',
+				'MM' => 'sys_dmail_feuser_category_mm',
+			)
+		)
+	);
+	t3lib_extMgm::addTCAcolumns('fe_users', $tempColumns, 1);
+	t3lib_extMgm::addToAllTCAtypes('fe_users', '--div--;Direct mail, module_sys_dmail_html, module_sys_dmail_category');
+}
 
 
 
--- ext_tables.sql
+++ ext_tables.sql
@@ -4,6 +4,16 @@
 CREATE TABLE fe_users (
 	mailhash varchar(60) DEFAULT '',
 
+	gender int(11) unsigned DEFAULT '0' NOT NULL,
+	date_of_birth int(11) DEFAULT '0' NOT NULL,
+	language char(2) DEFAULT '' NOT NULL,
+	zone varchar(45) DEFAULT '' NOT NULL,
+	static_info_country char(3) DEFAULT '' NOT NULL,
 	mobilephone varchar(20) DEFAULT '' NOT NULL,
 	gtc tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	status int(11) unsigned DEFAULT '0' NOT NULL,
+	by_invitation tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	comments text NOT NULL,
+	module_sys_dmail_html tinyint(3) unsigned DEFAULT '0' NOT NULL
+	module_sys_dmail_category int(10) unsigned DEFAULT '0' NOT NULL,
 );
