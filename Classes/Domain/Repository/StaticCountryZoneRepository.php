bf87e6c949fb5f933b07a0045f5b1e6e1052ccc9
sfischer <sfischer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-30 20:55:31 +0000

    Add static country zone viewhelper
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sf_register/trunk@47163 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/Domain/Repository/StaticCountryZoneRepository.php
@@ -0,0 +1,44 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2011 Sebastian Fischer <typo3@evoweb.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+class Tx_SfRegister_Domain_Repository_StaticCountryZoneRepository extends Tx_Extbase_Domain_Repository_FrontendUserRepository {
+	/**
+	 * Find all countries despecting the storage page
+	 *
+	 * @param string $iso2
+	 * @return integer
+	 */
+	public function findAllByIso2($iso2) {
+		$query = $this->createQuery();
+		$query->getQuerySettings()->setRespectStoragePage(FALSE);
+
+		return $query
+			->matching(
+				$query->equals('zn_country_iso_2', $iso2)
+			)
+			->execute();
+	}
+}
+
+?>
