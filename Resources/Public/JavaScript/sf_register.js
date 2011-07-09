d38900576dbf58b0b9177fbbd18fdf1b3fda6c0d
sfischer <sfischer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-07-09 15:20:42 +0000

    Add semicolon to lines Add var to variable definition
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sf_register/trunk@49654 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Public/JavaScript/sf_register.js
+++ Resources/Public/JavaScript/sf_register.js
@@ -12,7 +12,7 @@ function callTestPassword() {
 	var meter = testPassword(this.value);
 
 		// calculating percent score for sprite
-	percentScore = Math.min((Math.floor((meter.intScore / 3.4)) * 10), 100);
+	var percentScore = Math.min((Math.floor((meter.intScore / 3.4)) * 10), 100);
 
 		// displaying the sprite
 	document.getElementById("bargraph").className = 'is' + percentScore;
