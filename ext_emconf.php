9b526547c7bddd5ed0b6ff6e5c06c1fb1b85e1da
sfischer <sfischer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-06 10:52:18 +0000

    Lower requirements for branch 1.4.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sf_register/branches/1.4.0@56072 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,9 +35,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '4.6.0-0.0.0',
-			'extbase' => '1.4.0-0.0.0',
-			'fluid' => '1.4.0-0.0.0',
+			'typo3' => '4.5.0-0.0.0',
+			'extbase' => '1.3.0-0.0.0',
+			'fluid' => '1.3.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
