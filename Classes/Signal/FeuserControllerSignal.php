9ff9257a3e8662d129ec74264c4f5fad247f036f
Sebastian Fischer <typo3@evoweb.de>
2014-11-03 11:22:37 +0100

    [BUGFIX] Add empty slot to satisfy signal registered in localconf
    Resolves: #62640
    Releases: master
    
    Change-Id: Ib1f6b5621499d3a9cdb6043db6626a85c1709c90
    Reviewed-on: http://review.typo3.org/33741
    Reviewed-by: Sebastian Fischer <typo3@evoweb.de>
    Tested-by: Sebastian Fischer <typo3@evoweb.de>

---
+++ Classes/Signal/FeuserControllerSignal.php
@@ -0,0 +1,16 @@
+<?php
+namespace Evoweb\SfRegister\Signal;
+
+/**
+ * Class FeuserControllerSignal
+ *
+ * @package Evoweb\SfRegister\Signal
+ */
+class FeuserControllerSignal {
+	/**
+	 * @return void
+	 */
+	public function initializeAction() {
+		// @todo implement redirect if not logged in
+	}
+}
