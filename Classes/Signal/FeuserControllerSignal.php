d37f9c3056e4ee51752089ca0a44b9b23a6478ce
Sebastian Fischer <typo3@evoweb.de>
2014-11-03 17:35:53 +0100

    [FEATURE] Implement redirect if user is not logged in
    Releases: master
    Change-Id: I24e991f80e915be947586593262f7f0a3e2e9458
    Reviewed-on: http://review.typo3.org/33753
    Reviewed-by: Sebastian Fischer <typo3@evoweb.de>
    Tested-by: Sebastian Fischer <typo3@evoweb.de>

--- Classes/Signal/FeuserControllerSignal.php
+++ Classes/Signal/FeuserControllerSignal.php
@@ -1,5 +1,29 @@
 <?php
 namespace Evoweb\SfRegister\Signal;
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2014 Sebastian Fischer <typo3@evoweb.de>
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Class FeuserControllerSignal
@@ -8,9 +32,49 @@ namespace Evoweb\SfRegister\Signal;
  */
 class FeuserControllerSignal {
 	/**
+	 * @param \Evoweb\SfRegister\Controller\FeuserController $controller
+	 * @param array $settings
 	 * @return void
 	 */
-	public function initializeAction() {
-		// @todo implement redirect if not logged in
+	public function initializeAction($controller, $settings) {
+		if (!$this->userIsLoggedIn()) {
+			$redirectSettings = $settings['redirectSignal'];
+
+			if ((int)$redirectSettings['page']) {
+				$this->redirectToPage($redirectSettings['page']);
+			} elseif ($redirectSettings['controller']) {
+				$controller->forward($redirectSettings['action'], $redirectSettings['controller']);
+			} else {
+				$controller->forward($redirectSettings['action']);
+			}
+		}
 	}
+
+	/**
+	 * Checks if an user is logged in
+	 *
+	 * @return bool
+	 */
+	public function userIsLoggedIn() {
+		/** @var \TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication $frontendUser */
+		$frontendUser = $GLOBALS['TSFE']->fe_user;
+		return is_array($frontendUser->user);
+	}
+
+	/**
+	 * Redirect to a page with given id
+	 *
+	 * @param integer $pageId
+	 * @return void
+	 */
+	protected function redirectToPage($pageId) {
+		/** @var \TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder $uriBuilder */
+		$uriBuilder = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager')
+			->get('TYPO3\\CMS\\Extbase\\Mvc\\Web\\Routing\\UriBuilder');
+		$url = $uriBuilder
+			->setTargetPageUid($pageId)
+			->build();
+		\TYPO3\CMS\Core\Utility\HttpUtility::redirect($url);
+	}
+
 }
