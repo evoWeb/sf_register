b548b2ae3cf38fe1d178ff8af068bac8ae89a888
Sebastian Fischer <typo3@evoweb.de>
2015-07-10 09:14:02 +0200

    [BUGFIX] Remove use of ExtensionManagerUtility
    Change-Id: Ib1ac65fc9bf9c78bea8c7272ea4f6a9f86354c57
    Reviewed-on: http://review.typo3.org/41079
    Reviewed-by: Sebastian Fischer <typo3@evoweb.de>
    Tested-by: Sebastian Fischer <typo3@evoweb.de>

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,10 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
-
-ExtensionManagementUtility::addPlugin(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
 	array('LLL:EXT:sf_register/Resources/Private/Language/locallang_be.xml:tt_content.list_type_form', 'sfregister_form'),
 	'list_type',
 	'sf_register'
@@ -13,6 +10,6 @@ ExtensionManagementUtility::addPlugin(
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['sfregister_form'] = 'layout, select_key';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['sfregister_form'] = 'pi_flexform';
 
-ExtensionManagementUtility::addPiFlexFormValue(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
 	'sfregister_form', 'FILE:EXT:sf_register/Configuration/FlexForms/form.xml'
 );
