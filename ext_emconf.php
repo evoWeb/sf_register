7a67198fd97ed99609a11d170aeb0f3cd7f30195
Sebastian Fischer <typo3@evoweb.de>
2015-07-17 12:37:27 +0200

    [TASK] Modify version number of core to be compatible to
    Releases: master
    Change-Id: I77f9f080862bd141a92b0200ee7bf21cf37cfccc
    Reviewed-on: http://review.typo3.org/41556
    Reviewed-by: Sebastian Fischer <typo3@evoweb.de>
    Tested-by: Sebastian Fischer <typo3@evoweb.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,9 +25,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
-			'extbase' => '6.2.0-6.2.99',
-			'fluid' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.9.99',
+			'extbase' => '6.2.0-7.9.99',
+			'fluid' => '6.2.0-7.9.99',
 		),
 		'conflicts' => array(
 		),
