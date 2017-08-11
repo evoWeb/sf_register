56b250ba4b99953282d43eddea41863dfd37280e
Sebastian Fischer <typo3@evoweb.de>
2017-08-11 07:47:57 +0200

    Improve javascript

--- Resources/Public/JavaScript/sf_register.js
+++ Resources/Public/JavaScript/sf_register.js
@@ -3,9 +3,9 @@
 		define('map', ['window'], factory) :
 		factory(window);
 })(function(window) {
-	var document = window.document;
+	var document = window.document,
+		module = {},
 
-	var module = {},
 		loading = false,
 		zone,
 		zoneEmpty,
@@ -13,27 +13,60 @@
 		ajaxRequest;
 
 	/**
+	 * @param {String} id
+	 * @returns {Element}
+	 */
+	module.getElement = function (id) {
+		return 'object' === typeof id ? id : document.getElementById(id);
+	};
+
+	/**
+	 * @param {Element} element
+	 */
+	module.showElement = function (element) {
+		element.style.display = 'block';
+	};
+
+	/**
+	 * @param {Element} element
+	 */
+	module.hideElement = function (element) {
+		element.style.display = 'none';
+	};
+
+	/**
 	 * Attach an event to an element
 	 *
-	 * @param {string|object} id
-	 * @param {string} eventName
-	 * @param {function} callback
+	 * @param {String|Object} id
+	 * @param {String} eventName
+	 * @param {Function} callback
 	 */
-	function attachToElement(id, eventName, callback) {
-		var element = 'object' === typeof id ? id : document.getElementById(id);
+	module.attachToElement = function (id, eventName, callback) {
+		var element = module.getElement(id);
 
 		if (element && element.addEventListener) {
 			element.addEventListener(eventName, callback, false);
 		} else if (element) {
 			element.attachEvent('on' + eventName, callback);
 		}
-	}
+	};
 
-	function callTestPassword() {
-		var bargraph = document.getElementById('bargraph'),
-			meter = testPassword(this.value),
+
+	/**
+	 * Gets bargraph element and calls test password function
+	 * with value entered in field where this callback is attached
+	 *
+	 * @return void
+	 */
+	module.callTestPassword = function () {
+		var bargraph = module.getElement('bargraph'),
+
 			// calculating percent score for sprite
-			percentScore = Math.min((Math.floor((meter.intScore / 3.4)) * 10), 100) / 10,
+			meter = window.testPassword(this.value),
+			percentScore = Math.min(
+				(Math.floor(meter.intScore / 3.4) * 10),
+				100
+			) / 10,
 
 			// displaying the sprite
 			count = 0,
@@ -41,21 +74,27 @@
 
 		for (var blindKey in blinds) {
 			if (blinds.hasOwnProperty(blindKey)) {
-				blinds[blindKey].style.display = count < percentScore ? 'none' : 'inherit';
+				if (count < percentScore) {
+					module.hideElement(blinds[blindKey]);
+				} else {
+					module.showElement(blinds[blindKey]);
+				}
 				count++;
 			}
 		}
-	}
+	};
+
 
 	/**
 	 * Change value of zone selectbox
 	 *
 	 * @param event
 	 */
-	function changeZone(event) {
+	module.changeZone = function (event) {
 		if ((
-				(event.type === 'keyup' && event.keyCode === 40)
-				|| (event.type === 'keyup' && event.keyCode === 38)
+				(event.type === 'keyup' && (
+					event.keyCode === 40 || event.keyCode === 38
+				))
 				|| event.type === 'change'
 			)
 			&& loading !== true
@@ -65,35 +104,50 @@
 			var countrySelectedValue = target.options[target.selectedIndex].value;
 
 			zone.length = 0;
-			zone.style.display = 'none';
+			module.hideElement(zone);
 
-			zoneEmpty.style.display = 'none';
-			zoneLoading.style.display = 'block';
+			module.hideElement(zoneEmpty);
+			module.showElement(zoneLoading);
 
 			ajaxRequest = new XMLHttpRequest();
-			ajaxRequest.onreadystatechange = XHRResponse;
+			ajaxRequest.onreadystatechange = module.xhrReadyStateChanged;
 			ajaxRequest.open('POST', 'index.php?eID=sf_register');
 			ajaxRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 			ajaxRequest.send('tx_sfregister[action]=zones&tx_sfregister[parent]=' + countrySelectedValue);
 		}
-	}
+	};
 
-	function XHRResponse() {
-		if (ajaxRequest.readyState === 4 && ajaxRequest.status === 200) {
-			ajaxRequest.responseJSON = JSON.parse(ajaxRequest.responseText);
-			zoneLoading.style.display = 'none';
+	/**
+	 * Process ajax response and display error message or
+	 * hand data received to add zone option function
+	 *
+	 * @return void
+	 */
+	module.xhrReadyStateChanged = function (stateChanged) {
+		var xhrResponse = stateChanged.target;
 
-			if (ajaxRequest.responseJSON.status === 'error' || ajaxRequest.responseJSON.data.length === 0) {
-				zoneEmpty.style.display = 'block';
+		if (xhrResponse.readyState === 4 && xhrResponse.status === 200) {
+			var xhrResponseData = JSON.parse(xhrResponse.responseText);
+			module.hideElement(zoneLoading);
+
+			if (xhrResponseData.status === 'error' || xhrResponseData.data.length === 0) {
+				module.showElement(zoneEmpty);
 			} else {
-				XHRResponseSuccess(ajaxRequest.responseJSON.data);
+				module.addZoneOptions(xhrResponseData.data);
 			}
 		}
 
 		loading = false;
-	}
+	};
 
-	function XHRResponseSuccess(data) {
+	/**
+	 * Process data received with xhr response
+	 *
+	 * @param {Array} data
+	 *
+	 * @constructor
+	 */
+	module.addZoneOptions = function (data) {
 		for (var pointer = 0; pointer < data.length; pointer++) {
 			var option = document.createElement('option');
 			option.text = data[pointer].label;
@@ -102,34 +156,32 @@
 			zone.options[pointer] = option;
 		}
 
-		zone.style.display = 'block';
-	}
+		module.showElement(zone);
+	};
 
-	function uploadFile() {
+	/**
+	 * Adds a preview information about file to upload in a label
+	 *
+	 * @return void
+	 */
+	module.uploadFile = function () {
 		document.getElementById('uploadFile').value = this.value;
-	}
-
-	function attachEvents() {
-		attachToElement('sfrpassword', 'keyup', callTestPassword);
-
-		zone = document.getElementById('sfrZone');
-		zoneEmpty = document.getElementById('sfrZone_empty');
-		zoneLoading = document.getElementById('sfrZone_loading');
-
-		attachToElement('sfrCountry', 'change', changeZone);
-		attachToElement('sfrCountry', 'keyup', changeZone);
-		attachToElement('uploadButton', 'change', uploadFile);
-	}
+	};
 
 	/**
 	 * Attach content loaded element with callback to document
 	 */
 	function initialize() {
-		attachToElement(document, 'DOMContentLoaded', function () {
-			attachEvents();
+		module.attachToElement(document, 'DOMContentLoaded', function () {
+			zone = module.getElement('sfrZone');
+			zoneEmpty = module.getElement('sfrZone_empty');
+			zoneLoading = module.getElement('sfrZone_loading');
+
+			module.attachToElement('sfrpassword', 'keyup', module.callTestPassword);
+			module.attachToElement('sfrCountry', 'change', module.changeZone);
+			module.attachToElement('sfrCountry', 'keyup', module.changeZone);
+			module.attachToElement('uploadButton', 'change', module.uploadFile);
 		});
 	}
 	initialize();
-
-	return module;
 });
