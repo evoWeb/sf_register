d23623600d9674529e3f53f404843eddb301e161
Sebastian Fischer <typo3@evoweb.de>
2018-12-29 09:33:07 +0100

    Modify test config

--- .Build/run_functional_tests.sh
+++ .Build/run_functional_tests.sh
@@ -2,18 +2,21 @@
 
 echo "Running functional tests";
 
-export PHP_IDE_CONFIG="serverName=www.dev8.lan";
-export TYPO3_PATH_ROOT="/home/www/dev8/www/.Build/Web/";
+IP="127.0.0.1";
+export PHP_IDE_CONFIG="serverName=www.sf-register.lan";
+export TYPO3_PATH_APP="${PWD}/Web/";
+export TYPO3_PATH_ROOT="${PWD}/Web/";
 
-export typo3DatabaseName="typo3";
-export typo3DatabaseHost="localhost";
-export typo3DatabaseUsername="typo3";
-export typo3DatabasePassword="t_dev";
+export typo3DatabaseName="functional";
+export typo3DatabaseDriver="pdo_sqlite";
 
 /usr/bin/php \
+    -dxdebug.remote_enable=1 \
+    -dxdebug.remote_mode=req \
+    -dxdebug.remote_port=9000 \
     -dxdebug.remote_autostart=1 \
-    -dxdebug.remote_host=127.0.0.1 \
-    ../../../../../bin/phpunit \
+    -dxdebug.remote_host=${IP} \
+    ./bin/phpunit \
     --colors \
-    -c ../../../../../vendor/typo3/testing-framework/Resources/Core/Build/FunctionalTests.xml \
+    -c ./Web/vendor/typo3/testing-framework/Resources/Core/Build/FunctionalTests.xml \
     ../Tests/Functional/Controller/FeuserPasswordControllerTest.php
