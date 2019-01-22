6f01e563f7411230b32300a0637689fec2a86470
Sebastian Fischer <typo3@evoweb.de>
2019-01-22 18:38:33 +0100

    Open up return types to be able to render both strings and arrays

--- Classes/Services/Captcha/AbstractAdapter.php
+++ Classes/Services/Captcha/AbstractAdapter.php
@@ -44,9 +44,9 @@ abstract class AbstractAdapter implements \Evoweb\SfRegister\Interfaces\CaptchaI
     /**
      * Renders the output of an concrete captcha
      *
-     * @return array
+     * @return string|array
      */
-    abstract public function render(): array;
+    abstract public function render();
 
     /**
      * Returns if the result of the validation was valid or not
