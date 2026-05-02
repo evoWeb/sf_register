6344646ba78cc55f6577ef0b63d8e99dd760fbee
Sebastian Fischer <typo3@evoweb.de>
2026-05-02 11:04:58 +0200

    [TASK] Degrade moduleSysDmailCategory to be a single reference property
    The property is not used by shipped configuration and is only available for backward compatibility

--- Classes/Controller/FeuserController.php
+++ Classes/Controller/FeuserController.php
@@ -207,7 +207,6 @@ class FeuserController extends ActionController
 
         $configuration->allowProperties(...($this->settings['fields']['selected'] ?? []));
 
-        $configuration->forProperty('moduleSysDmailCategory')->allowAllProperties();
         $configuration->forProperty('image')->allowAllProperties();
         $configuration->setTypeConverterOption(
             PersistentObjectConverter::class,
