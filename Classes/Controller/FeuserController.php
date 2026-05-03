2fb6626b431df6cb92f7326874a08e36832ff6c7
Sebastian Fischer <typo3@evoweb.de>
2026-05-03 13:08:34 +0200

    [TASK] Remove allowAllProperties for image property
    After removing this, the upload still works.

--- Classes/Controller/FeuserController.php
+++ Classes/Controller/FeuserController.php
@@ -207,7 +207,6 @@ class FeuserController extends ActionController
 
         $configuration->allowProperties(...($this->settings['fields']['selected'] ?? []));
 
-        $configuration->forProperty('image')->allowAllProperties();
         $configuration->setTypeConverterOption(
             PersistentObjectConverter::class,
             (string)PersistentObjectConverter::CONFIGURATION_CREATION_ALLOWED,
