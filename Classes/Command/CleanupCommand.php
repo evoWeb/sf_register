f9f907f1322708becefb6e40fccbd0a73323439e
Rico Sonntag <rico.sonntag@netresearch.de>
2026-08-19 10:31:01 +0200

    [BUGFIX] Cleanup command: actually delete disabled unconfirmed users
    findInOutdatedTemporaryUsers() queries fe_users through the default
    restriction container, whose HiddenRestriction forces disable = 0.
    Accounts that never confirmed the double opt-in are disable = 1, so they
    were never matched and the command deleted nothing.
    
    Remove only the HiddenRestriction via
    getRestrictions()->removeByType(HiddenRestriction::class), keeping the
    DeletedRestriction (the command hard-deletes) and the time restrictions.
    
    Also fix the orphan-image reference cleanup, which read non-existent keys
    ($user['fe_users'] / $user['image']) from a row that only contains uid.

--- Classes/Command/CleanupCommand.php
+++ Classes/Command/CleanupCommand.php
@@ -24,6 +24,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Database\Query\Restriction\HiddenRestriction;
 use TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException;
 use TYPO3\CMS\Core\Resource\Exception\FileOperationErrorException;
 use TYPO3\CMS\Core\Resource\Exception\InsufficientFileAccessPermissionsException;
@@ -96,6 +97,7 @@ class CleanupCommand extends Command
     {
         $table = 'fe_users';
         $queryBuilder = $this->getQueryBuilderForTable($table);
+        $queryBuilder->getRestrictions()->removeByType(HiddenRestriction::class);
 
         $result = $queryBuilder
             ->select('uid')
@@ -161,8 +163,8 @@ class CleanupCommand extends Command
             ->getConnection()
             ->delete($table, [
                 'uid_foreign' => $user['uid'],
-                'tablenames' => $user['fe_users'],
-                'fieldname' => $user['image'],
+                'tablenames' => 'fe_users',
+                'fieldname' => 'image',
             ]);
     }
 
